60872ff922cfee13a28842c9ac436ee7bbb8fb30
Éricles Miller <ericlesmiller15@gmail.com>
2026-05-26 15:39:00 -0300

    feat: create connection with database using pgx

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"net/http"
 	"time"
-
+	"github.com/joho/godotenv"
 	"github.com/Ericles-Miller/crudUserInMemory/api"
+	"github.com/Ericles-Miller/crudUserInMemory/internal/database"
+
 )
 
 func main() {
+	godotenv.Load()
 	if err := run(); err != nil {
 		slog.Error("Failed to execute code", "error", err)
 	}
@@ -17,12 +21,18 @@ func main() {
 }
 
 func run() error {
+	ctx := context.Background()
+
+	pool, err := database.ConnectDB(ctx)
+	if err != nil {
+		return err
+	}
+	defer pool.Close()
 
 	handler := api.NewHandler()
 
 	s := http.Server{
-		ReadTimeout: 5 *
-			time.Second,
+		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		Addr:         ":8080",
 		Handler:      handler,
